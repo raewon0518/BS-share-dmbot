4b8c290d795a58ed905bdf6788e58b3d436c9bd9
raewon0518 <68143504+raewon0518@users.noreply.github.com>
2020-07-11 22:49:49 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -12,7 +12,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('계정거래방 운영중')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -25,13 +25,13 @@ async def on_message(message):
             else:
                 try:
                     msg = message.content[4:]
-                    if message.author.id == ★자신의 디스코드ID를 적어주세요(퍼미션):
-                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="★★최상단 제목★★")
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text=f"discord.gg/★★서버초대코드★★")
+                    if message.author.id == :686587812652253191
+                        embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="계정거래방 KR")
+                        embed.add_field(name="계정거래방 DM", value=msg, inline=True)
+                        embed.set_footer(text=f"이 메시지는 계정거래방 KR 에서 발송 했습니다")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzMxNTA3MTA3MTE4OTcyOTY5.XwnDRw.nTi2AxnNz3qKd-fWH6ogWuY0JBY')
